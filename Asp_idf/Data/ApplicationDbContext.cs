d65a647dca9c6c3f7c7d1ed97d4a530792d5d6b8
Angel Vasilev 10a <angelvasilev22A@gmail.com>
2026-05-10 17:40:26 +0300

    Initial library project

---
+++ Asp_idf/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Asp_idf.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp_idf.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Asp_idf/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace Asp_idf.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        // Foreign key
+        public int AuthorId { get; set; }
+
+        // Navigation property
+        public Author? Author { get; set; }
+    }
+}
